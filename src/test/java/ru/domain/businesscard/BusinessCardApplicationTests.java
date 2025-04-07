d5cdb759ba83e36e142851ad63d04d0eda3e8cea
Vadsxd <vadim.sannikov.2018@mail.ru>
2025-04-07 18:46:54 +0300

    init

---
+++ src/test/java/ru/domain/businesscard/BusinessCardApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.domain.businesscard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
